8b7eb9360d87659842681538f368bd47ac0f9eb3
Andres <andresanchez1604@gmail.com>
2025-10-15 12:19:42 -0400

    adding icon listing, and using pokemon home sprites instead

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,6 +1,10 @@
 const infoDiv = document.querySelector(".pkmn-info");
 const form = document.getElementById("search-form");
 const input = document.getElementById("input-search-pkmn");
+const listView = document.querySelector(".display-list");
+const mosaicView = document.querySelector(".display-mosaic");
+const radioList = document.getElementById("list-view");
+const radioMosaic = document.getElementById("mosaic-view");
 
 form.addEventListener("submit", async e => {
     e.preventDefault();
@@ -28,3 +32,13 @@ async function fetchPokemon(query) {
         infoDiv.innerHTML = "<p>Pokémon not found ⚠️</p>";
     }
 }
+
+radioList.addEventListener("change", () => {
+  listView.classList.remove("hidden");
+  mosaicView.classList.add("hidden");
+});
+
+radioMosaic.addEventListener("change", () => {
+  mosaicView.classList.remove("hidden");
+  listView.classList.add("hidden");
+});
